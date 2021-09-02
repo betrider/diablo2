568e6904c38d48d7136b26b1bb2940c4f764be60
betrider <betrider@example.com>
2021-09-02 23:17:18 +0900

    번역 추가

--- lib/lang/en_us.dart
+++ lib/lang/en_us.dart
@@ -7,9 +7,66 @@ const Map<String, String> enUS = {
   'exchange(standard)': 'exchange(standard)',
   'exchange(ladder)': 'exchange(ladder)',
   'welcome to the diablo2 exchange': 'welcome to the diablo2 exchange',
-  'item dictionary': 'item dictionary'
+  'item dictionary': 'item dictionary',
 
   //아이템
 
   //아이템 옵션
+
+  //잼
+  'Perfect Ruby':'Perfect Ruby',
+  'Perfect Sapphire':'Perfect Sapphire',
+  'Perfect Topaz':'Perfect Topaz',
+  'Perfect Emerald':'Perfect Emerald',
+  'Perfect Diamond':'Perfect Diamond',
+  'Perfect Amethyst':'Perfect Amethyst',
+  'Perfect Skull':'Perfect Skull',
+
+  //주얼
+  'Jewel':'Jewel',
+
+  //참
+  'Grand Charms':'Grand Charms',
+  'Large Charms':'Large Charms',
+  'Small Charms':'Small Charms',
+
+  'Hellfire Toach':'Hellfire Toach',
+  'Annihilus':' Annihilus',
+
+  //룬  
+  'Rune':'Rune',
+
+  'El':'El',
+  'Eld':'Eld',
+  'Tir':'Tir',
+  'Nef':'Nef',
+  'Eth':'Eth',
+  'Ith':'Ith',
+  'Tal':'Tal',
+  'Ral':'Ral',
+  'Ort':'Ort',
+  'Thul':'Thul',
+  'Amn':'Amn',
+  'Sol':'Sol',
+  'Shael':'Shael',
+  'Dol':'Dol',
+  'Hel':'Hel',
+  'Io':'Io',
+  'Lum':'Lum',
+  'Ko':'Ko',
+  'Fal':'Fal',
+  'Lem':'Lem',
+  'Pul':'Pul',
+  'Um':'Um',
+  'Mal':'Mal',
+  'Ist':'Ist',
+  'Gul':'Gul',
+  'Vex':'Vex',
+  'Ohm':'Ohm',
+  'Lo':'Lo',
+  'Sur':'Sur',
+  'Ber':'Ber',
+  'Jah':'Jah',
+  'Cham':'Cham',
+  'Zod':'Zod'
 };
--- lib/lang/ko_kr2.dart
+++ lib/lang/ko_kr2.dart
@@ -5,5 +5,66 @@ const Map<String, String> koKR2 = {
   'exchange(standard)': '거래소(스탠다드)',
   'exchange(ladder)': '거래소(래더)',
   'welcome to the diablo2 exchange': '디아블로2 거래소에 오신걸 환영합니다.',
-  'item dictionary': '아이템 사전'
+  'item dictionary': '아이템 사전',
+
+  //아이템
+
+  //아이템 옵션
+
+  //잼
+  'Perfect Ruby':'최상급 루비',
+  'Perfect Sapphire':'최상급 사파이어',
+  'Perfect Topaz':'최상급 토파즈',
+  'Perfect Emerald':'최상급 에메랄드',
+  'Perfect Diamond':'최상급 다이아몬드',
+  'Perfect Amethyst':'최상급 자수정',
+  'Perfect Skull':'최상급 스컬',
+
+  //주얼
+  'Jewel':'주얼',
+
+  //참
+  'Grand Charms':'그랜드 차암',
+  'Large Charms':'라지 차암',
+  'Small Charms':'스몰 차암',
+
+  'Hellfire Toach':'지옥의 횃불',
+  'Annihilus':'애니힐러스',
+
+  //룬
+  'Rune':'룬',
+
+  'El':'엘(El)',
+  'Eld':'엘드(Eld)',
+  'Tir':'티르(Tir)',
+  'Nef':'네프(Nef)',
+  'Eth':'에드(Eth)',
+  'Ith':'아이드(Ith)',
+  'Tal':'탈(Tal)',
+  'Ral':'랄(Ral)',
+  'Ort':'오르트(Ort)',
+  'Thul':'주울(Thul)',
+  'Amn':'앰(Amn)',
+  'Sol':'솔(Sol)',
+  'Shael':'샤에(Shael)',
+  'Dol':'돌(Dol)',
+  'Hel':'헬(Hel)',
+  'Io':'포(Io)',
+  'Lum':'룸(Lum)',
+  'Ko':'코(Ko)',
+  'Fal':'팔(Fal)',
+  'Lem':'렘(Lem)',
+  'Pul':'풀(Pul)',
+  'Um':'우움(Um)',
+  'Mal':'말(Mal)',
+  'Ist':'아이스트(Ist)',
+  'Gul':'굴(Gul)',
+  'Vex':'벡스(Vex)',
+  'Ohm':'옴(Ohm)',
+  'Lo':'로(Lo)',
+  'Sur':'수르(Sur)',
+  'Ber':'베르(Ber)',
+  'Jah':'자(Jah)',
+  'Cham':'참(Cham)',
+  'Zod':'조드(Zod)'
 };
